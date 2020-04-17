dd708d937e62d18ae4786fd7bbedf01c7e958218
rkant225 <rkant225@gmail.com>
2020-04-18 00:11:54 +0530

    Updates

---
+++ precache-manifest.32e81222884ac72c48505555f7eb020d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbbc8bdd92df73b92d72dcaa8ed17fcd",
+    "url": "/basic_react_js_concepts/index.html"
+  },
+  {
+    "revision": "1ce6a4d7cd5b13d87a81",
+    "url": "/basic_react_js_concepts/static/css/main.26713bd4.chunk.css"
+  },
+  {
+    "revision": "0fcabcd35344ac501380",
+    "url": "/basic_react_js_concepts/static/js/2.03dcd58e.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/basic_react_js_concepts/static/js/2.03dcd58e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ce6a4d7cd5b13d87a81",
+    "url": "/basic_react_js_concepts/static/js/main.5a2c686b.chunk.js"
+  },
+  {
+    "revision": "48647e3bdb10df780e30",
+    "url": "/basic_react_js_concepts/static/js/runtime-main.91a79cac.js"
+  }
+]);
